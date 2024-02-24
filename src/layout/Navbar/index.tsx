23443200314f4381ae2553495af1bfa3ed2a29d7
BhupeshChauhan <erchauhanbhupesh@gmail.com>
2024-02-24 17:34:29 +0530

    build fix

--- src/layout/Navbar/index.tsx
+++ src/layout/Navbar/index.tsx
@@ -1,35 +1,8 @@
 import Link from "next/link";
-import React, { useState } from "react";
-import { GoSearch } from "react-icons/go";
-import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
 import Logo from "@/Shared/Logo";
 
 const Navbar = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const search: any =
-    typeof searchParams.get("query") === "string"
-      ? searchParams.get("query")
-      : "";
-  const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
-  const handleKeyPress = (e: any) => {
-    let query = e.target.value;
-    if (e.keyCode === 13) {
-      if (query.length > 0) {
-        router.push(`/search?query=${query}`, { scroll: false });
-      } else {
-        router.push(`/`, { scroll: false });
-      }
-    }
-  };
-
-  const handleBlur = (e: any) => {
-    let query = e.target.value;
-    if (!(query.length > 0)) {
-      router.push(`/`, { scroll: false });
-    }
-  };
   return (
     <nav className="sticky top-3 z-30 bg-white bg-opacity-15">
       <div className="navbar  w-[90%] rounded-2xl shadow-md m-auto">
